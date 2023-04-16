9bd5d0b823835708d6ce64a6791d9a0322a04ccd
Lara Rodrigues La Moglie <laruu147@gmail.com>
2023-04-16 13:51:21 -0300

    updates

---
+++ scripts/vis_1.js
@@ -0,0 +1,54 @@
+// config. fecha español
+d3.json('https://cdn.jsdelivr.net/npm/d3-time-format@3/locale/es-ES.json').then(locale => {
+  d3.timeFormatDefaultLocale(locale)
+})
+
+d3.dsv(',', '147_desratizacion_act.csv', d3.autoType).then(data => {
+  data = data.filter(d => ['BALVANERA', 'PALERMO', 'SAAVEDRA',].includes(d.domicilio_barrio));
+  data = data.filter(d => ['LIMPIEZA Y RECOLECCIÓN'].includes(d.categoria));
+ 
+
+  const dayOfWeekMap = {
+    'Monday': 1,
+    'Tueday': 2,
+    'Wednesday': 3,
+    'Thursday': 4,
+    'Friday': 5,
+    'Saturday': 6,
+    'Sunday': 7
+  };
+
+  console.log(data.length)
+
+  let chart = Plot.plot({
+    width:1000,
+    height:400,
+    x: {
+      //domain: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" ]
+      //tickFormat: d => d3.timeFormat('%B')(new Date(2021, d - 1, 1))
+    },
+    y: {
+      domain: [0,30]
+    },
+    
+    marks: [
+      Plot.line(data, Plot.binY({y: "count"},{
+        x: "mes_prestacion",
+        stroke: "domicilio_barrio",
+        strokeWidth: 2,
+        z: "domicilio_barrio",
+        curve: "catmull-rom",
+      })),
+      Plot.dot(data, Plot.binY({y: "count"},{
+        x: "mes_prestacion",
+        z: "domicilio_barrio",
+        fill: "domicilio_barrio",
+        r:5,
+      }))
+    ],
+    color:{
+    }
+  })
+  // Agregamos chart al div#chart de index.html
+  d3.select('#vis_1').append(() => chart)
+})
