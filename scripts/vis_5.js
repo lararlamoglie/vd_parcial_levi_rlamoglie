4c8cb8dba95183109a95d89bce19f22d23d8d473
Lara Rodrigues La Moglie <laruu147@gmail.com>
2023-04-25 18:12:48 -0300

    5

--- scripts/vis_5.js
+++ scripts/vis_5.js
@@ -1,5 +1,5 @@
 const mapaFetch3 = d3.json('../../data/barrios-caba3.geojson')
-const dataFetch5 = d3.dsv(',', '../../data/147_desratizacion.csv', d3.autoType)
+const dataFetch5 = d3.dsv(',', 'https://lararlamoglie.github.io/vd_parcial_levi_rlamoglie/data/147_desratizacion.csv', d3.autoType)
   .then(data => data.filter(d => d.domicilio_barrio === "PALERMO"))
 
 Promise.all([mapaFetch3, dataFetch5]).then(([barrios, data]) => {
